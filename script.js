932f13b843555267067fb1b7c37a3a4b59c54611
Mettha <mettegrau@hotmail.com>
2022-11-01 13:46:38 +0100

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Christians typewriter 
+/* Christians typewriter 
+
 var messageArray = ["Hvad er dialog på tværs ?"];
 var textposition = 0;
 var speed = 100;
@@ -8,14 +9,38 @@ typewriter = () => {
   innerHTML = messageArray[0].substring(0,textposition) + "<span>\u25ae</span>";
 
   if(textposition++ != messageArray[0].length)
-  setTimeout(typewriter, speed);
+  setTimeout(typewriter, speed); 
+  else{
+      textposition = 0; 
+      setTimeout(typewriter, 1000);
+  }
 }
-window.addEventListener("load", typewriter);
-
+window.addEventListener("load", typewriter); 
+*/
 
 /*
-// Min typwriter 
+//Kims Nye typewriter
+var i = 0;
+var txt ='Hvad er dialog på tværs ?';
+var speed = 50;
 
+        function typeWriter() {
+            if (i < txt.length) {
+                document.getElementById("type").innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            } else { //else-delen skal kun med, hvis den skal nulstille/slette og gentage sig efter 2 sekunder......
+                i = 0;
+                document.getElementById("type").innerHTML = "";
+                setTimeout(typeWriter, 2000);
+            }
+        }
+        //  typeWriter = function(){};
+        typeWriter();
+*/
+
+
+// Mettes typewriter 
 var i = 0;
 var txt = 'Hvad er dialog på tværs ?';
 var speed = 300;
@@ -34,21 +59,8 @@ function typeWriter() {
 }
 
 typeWriter();
-*/
 
-/* (dette er kims kode)
-//afspil video når synlig
-var blooper = document.getElementById("myVideo");
- 
-window.onscroll = function() {
-  if (blooper.getBoundingClientRect().bottom < 250 || blooper.getBoundingClientRect().top > 250)
-  blooper.pause();
-else
-  blooper.play();
-}
-*/
 
-/*
 //afspil video når synlig
 // this function will check whether a tag is visible
 function elFllVsbl(el) {
@@ -64,21 +76,36 @@ window.addEventListener("scroll", function () {
     else
       myVid.pause();
   });
-*/
-//second try eventlistner
-/*
-  var elem = document.getElementById("myVideo");
-   
 
-  window.onscroll = function(){
-   if (elF11Vsbl(elem))
-     elem.play();
-   else
-     elem.pause();
+
+   /*
+//2th try eventlistner
+   var video = document.getElementById('myVideo'), fraction = 0.8;
+
+   function checkScroll() {
+       var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+       b = y + h, //bottom
+       visibleX, visibleY, visible;
+
+       visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+       visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+       visible = visibleX * visibleY / (w * h);
+
+       if (visible > fraction) {
+           video.play();
+       } else {
+           video.pause();
+       }
    }
-  */
+
+   checkScroll();
+   window.addEventListener('scroll', checkScroll, false);
+   window.addEventListener('resize', checkScroll, false);
+/*
 
 /* is element visible? if ok skift farve */
+/*
 var videoelementet = document.getElementById("myVideo");
 var img2 = document.getElementById("img1");
 window.addEventListener("scroll", function () {
@@ -93,7 +120,35 @@ window.addEventListener("scroll", function () {
     if (elFllVsbl(img1)) {
         img1.style.filter = "grayscale(0%)";
     } else {
-        img1.style.filter = "grayscale(100%)";
+        img1.style.filter = "grayscale(100%)"; 
     }
-}); // end eventlistener
+}); // end eventlistener 
+*/
 
+// slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
