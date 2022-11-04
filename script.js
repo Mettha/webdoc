d9d4894ff3fecd0cd15a6fc4e73d7a909d9e9dea
Mettha <mettegrau@hotmail.com>
2022-11-04 11:37:23 +0100

    finale

--- script.js
+++ script.js
@@ -19,27 +19,6 @@ window.addEventListener("load", typewriter);
 
 
 /*
-//Kims Nye typewriter
-var i = 0;
-var txt ='Hvad er dialog på tværs ?';
-var speed = 50;
-
-        function typeWriter() {
-            if (i < txt.length) {
-                document.getElementById("type").innerHTML += txt.charAt(i);
-                i++;
-                setTimeout(typeWriter, speed);
-            } else { //else-delen skal kun med, hvis den skal nulstille/slette og gentage sig efter 2 sekunder......
-                i = 0;
-                document.getElementById("type").innerHTML = "";
-                setTimeout(typeWriter, 2000);
-            }
-        }
-        //  typeWriter = function(){};
-        typeWriter();
-*/
-
-/*
 // Mettes typewriter 
 var i = 0;
 var txt = 'Hvad er dialog på tværs ?';
@@ -61,6 +40,7 @@ function typeWriter() {
 typeWriter();
 */
 
+/*
 //afspil video når synlig
 // this function will check whether a tag is visible
 function elFllVsbl(el) {
@@ -76,53 +56,6 @@ window.addEventListener("scroll", function () {
     else
       myVid.pause();
   });
-
-
-   /*
-//2th try eventlistner
-   var video = document.getElementById('myVideo'), fraction = 0.8;
-
-   function checkScroll() {
-       var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
-       b = y + h, //bottom
-       visibleX, visibleY, visible;
-
-       visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-       visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
-
-       visible = visibleX * visibleY / (w * h);
-
-       if (visible > fraction) {
-           video.play();
-       } else {
-           video.pause();
-       }
-   }
-
-   checkScroll();
-   window.addEventListener('scroll', checkScroll, false);
-   window.addEventListener('resize', checkScroll, false);
-/*
-
-/* is element visible? if ok skift farve */
-/*
-var videoelementet = document.getElementById("myVideo");
-var img2 = document.getElementById("img1");
-window.addEventListener("scroll", function () {
-    if (elFllVsbl(videoelementet)) {
-        console.log("play video");
-        player.playVideo(); // with player you can manipulate the video
-    } else {
-        console.log("pause video");
-        player.pauseVideo();
-    }
-
-    if (elFllVsbl(img1)) {
-        img1.style.filter = "grayscale(0%)";
-    } else {
-        img1.style.filter = "grayscale(100%)"; 
-    }
-}); // end eventlistener 
 */
 
 // slideshow
